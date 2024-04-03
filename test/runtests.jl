4f1661535e9cf6c19685f13b4f198dfbdf84795a
DislikePineapple <sheng_y@tju.edu.cn>
2024-04-03 11:19:33 +0800

    reformat

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ const is_APPVEYOR = Sys.iswindows() && haskey(ENV, "APPVEYOR")
 @time begin
     if GROUP == "Core" || GROUP == "All"
         @time @safetestset "Aqua" include("aqua.jl")
+        @time @safetestset "format" include("format.jl")
         @time @safetestset "Nonlinear" include("nonlinear_test.jl")
         @time @safetestset "Linear ODE" include("linear_ode_test.jl")
         @time @safetestset "BVP Blasius" include("blasius.jl")
