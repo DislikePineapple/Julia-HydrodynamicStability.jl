d26d92148f14a03e5ca15160befe9932cebb9eb9
DislikePineapple <sheng_y@tju.edu.cn>
2024-04-27 19:20:51 +0800

    update doc

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,3 +1,7 @@
-# Put in a separate page so it can be used by SciMLDocs.jl
+# Put in a separate page so it can be used by HydrodynamicStability.jl
 
-pages = ["Home" => "index.md"]
+pages = [
+    "Home" => "index.md",
+    "ODE" => "ode.md",
+    "PDE" => "pde.md"
+]
